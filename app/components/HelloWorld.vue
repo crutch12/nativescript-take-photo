d1d283f6dece811c8da3aa796bbc597cbe52dfef
criitch <criitch@yandex.ru>
2019-04-29 23:33:17 +0300

    take photo

--- app/components/HelloWorld.vue
+++ app/components/HelloWorld.vue
@@ -1,34 +1,64 @@
 <template>
-    <Page class="page">
-        <ActionBar title="Home" class="action-bar" />
-        <ScrollView>
-            <StackLayout class="home-panel">
-                <!--Add your page content here-->
-                <Label textWrap="true" text="Play with NativeScript!" class="h2 description-label" />
-                <Label textWrap="true" text="Write code in the editor or drag and drop components to build a NativeScript mobile application." class="h2 description-label" />
-                <Label textWrap="true" text="Scan the QR code with your mobile device and watch the changes sync live while you play with the code." class="h2 description-label" />
-            </StackLayout>
-        </ScrollView>
-    </Page>
+	<Page class="page">
+		<ActionBar :title="`Фото ${takenPhotos.length ? takenPhotos.length : ''}`" class="action-bar" />
+    <StackLayout>
+      <Button :text="textPicture" class="btn btn-primary take-button" @tap="takePicture"></Button>
+      <ScrollView orientation="vertical">
+        <FlexboxLayout class="photos">
+          <StackLayout orientation="vertical" v-for="(photo, idx) in takenPhotos" :key="idx">
+            <TextView editable="false" :text="`Время создания: ${photo.time}` "/>
+            <Image :src="photo.src" class="photo"></Image>
+          </StackLayout>
+        </FlexboxLayout>
+      </ScrollView>
+    </StackLayout>
+	</Page>
 </template>
 
 <script>
-export default {
+  import * as camera from 'nativescript-camera';
+  import * as http from 'http';
+  import * as moment from 'moment';
+
+  export default {
     data () {
-        return {
-        };
+      return {
+        takenPhotos: [],
+        textPicture: 'Сделать фото',
+        moment: moment,
+      };
     },
-}
+    methods: {
+      takePicture() {
+        // See the options at https://github.com/NativeScript/nativescript-camera#using-the-options-to-take-memory-efficient-picture
+        // for more information on how to customize the pictures you take.
+        camera
+          .takePicture({ width: 300, height: 300, keepAspectRatio: true })
+          .then(imageAsset => {
+            this.takenPhotos.push({
+              src: imageAsset,
+              time: moment().format('LT'),
+            })
+          })
+          .catch(err => {
+            console.error("Error -> " + err.message);
+          });
+      },
+    },
+  }
 </script>
 
-<style scoped>
-.home-panel {
-    vertical-align: center;
-    font-size: 20;
-    margin: 15;
-}
-
-.description-label {
-    margin-bottom: 15;
-}
+<style lang="scss" scoped>
+  .take-button {
+    margin-top: 20;
+  }
+  .photos {
+    flex-wrap: wrap;
+    padding: 20 10 20 10;
+  }
+  .photo {
+    border-radius: 10;
+    margin-bottom: 10;
+    width: 100%;
+  }
 </style>
